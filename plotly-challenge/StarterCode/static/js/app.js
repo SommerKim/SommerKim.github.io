b0da384975ed7a3d6612adf52a0dfd6903fde074
SommerKim <74261427+SommerKim@users.noreply.github.com>
2021-04-06 09:34:37 -0400

    built initial bar and bubble charts

--- plotly-challenge/StarterCode/static/js/app.js
+++ plotly-challenge/StarterCode/static/js/app.js
@@ -44,20 +44,73 @@
 
 // Establish variables and get data from json
 // these are the variables that name all similar data within the json-- all otu_ids, all sample_values
-d3.json("samples.json").then(data => {
-    console.log(data);
-    let id = unpack(data.samples, 0);
-    let otu_ids = unpack(data.samples, 1);
-    let sample_values = unpack(data.samples, 2);
-    let otu_labels = unpack(data.samples, 3);
-    let metadata = unpack(data.metadata);
-    ...
-    let dropdownMenu = d3.select("#selDataset");
-    let input = dropdownMenu.property("value");
-});
+function init() {
+    d3.json("samples.json").then(data => {
+        console.log(data);
+
+        // Get data
+        let id = unpack(data.samples, 0);
+        let otu_ids = unpack(data.samples, 1);
+        let sample_values = unpack(data.samples, 2);
+        let otu_labels = unpack(data.samples, 3);
+        let metadata = unpack(data.metadata);
+
+        // Build bar chart:
+        let barData = [{
+            x: sample_values,
+            y: otu_ids,
+            type: 'bar'
+        }];
+
+        let barLayout = {
+            title: 'Top 10 OTUs',
+        }
+
+        // Plot bar chart
+        Plotly.newPlot("bar", barData, barLayout)
+
+        // Build bubble chart:
+        let bubbleData = [{
+            x: otu_ids,
+            y: sample_values,
+            marker: {
+                color: otu_ids,
+                size: sample_values,
+            mode: 'markers'
+            };
+        }];
+
+        let bubbleLayout = {
+            title: 'Top 10 OTUs',
+        }
+
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout)
+        
+        ...
+        let dropdownMenu = d3.select("#selDataset");
+        let input = dropdownMenu.property("value");
+    })
+};
 
 
-
+var data = [{
+    x: [1999, 2000, 2001, 2002],
+    y: [10, 15, 13, 17],
+    type: 'scatter'
+  }];
+  
+  var layout = {
+    title: 'Sales Growth',
+    xaxis: {
+      title: 'Year',
+      showgrid: false,
+      zeroline: false
+    },
+    yaxis: {
+      title: 'Percent',
+      showline: false
+    }
+  
 // So: Start by finishing d3.json and drawing initial plots.
 // Go on to updateInfo and make sure all variables necessary for plotting are established
 // Build initial plots
