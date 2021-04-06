ceb6fe7dd380bbb9bc947b8781cc23999f4a89b0
SommerKim <74261427+SommerKim@users.noreply.github.com>
2021-04-06 09:14:57 -0400

    Variables established

--- plotly-challenge/StarterCode/static/js/app.js
+++ plotly-challenge/StarterCode/static/js/app.js
@@ -13,13 +13,21 @@
 
 // filter data from json to narrow down to id
 // this is the data to plot-- one specific otu_id, one specific sample_value
- updateInfo = (data, id) => {
+ updatePlots = (data, id) => {
     // let id = d3.event.target.value; // value of ddl
     let sample = data.samples.filter(sample => sample.id === id);
+
+    // Bubble chart variables
     let xBubble = sample.otu_ids;
     let yBubble = sample.sample_values;
+    let markerSize = sample.sample_values;
+    let markerColor = sample.otu_ids;
+    let textValue = sample.otu_labels;
+
+    // Bar chart variables
     let xBar = sample.sample_values;
     let yBar = sample.otu_ids;
+    let hovertext = sample.otu_labels;
 
 };
 
@@ -38,9 +46,11 @@
 // these are the variables that name all similar data within the json-- all otu_ids, all sample_values
 d3.json("samples.json").then(data => {
     console.log(data);
+    let id = unpack(data.samples, 0);
     let otu_ids = unpack(data.samples, 1);
     let sample_values = unpack(data.samples, 2);
     let otu_labels = unpack(data.samples, 3);
+    let metadata = unpack(data.metadata);
     ...
     let dropdownMenu = d3.select("#selDataset");
     let input = dropdownMenu.property("value");
@@ -50,4 +60,5 @@ d3.json("samples.json").then(data => {
 
 // So: Start by finishing d3.json and drawing initial plots.
 // Go on to updateInfo and make sure all variables necessary for plotting are established
+// Build initial plots
 // Then try to figure out restyle
